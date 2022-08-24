687a64a032241b4c622479230eeb8872606b05ec
Diego Palacios <diegopalacios3@gmail.com>
2022-08-24 23:24:49 +0200

    Search functionality

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -4,13 +4,11 @@ import { allUsersQuery } from "../../utils/queries";
 import { client } from "../../utils/client";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
   if(req.method === 'GET') {
     const query = allUsersQuery()
     const data = await client.fetch(query)
     
     if(data) {
-      console.log('data', data)
       res.status(200).json(data);
     } else {
       res.json([]);
